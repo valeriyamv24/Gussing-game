38ab75fc032b58cc96ec46c07597344c464154b4
Valerie Martinovitch <valeriyamv24@Gmail.com>
2022-02-11 14:44:06 -0600

    not a finel version

--- guessGame.js
+++ guessGame.js
@@ -1,48 +1,54 @@
-// alert('Welcome to the most amazing gussing game')
+function main() {
+alert("Welcome to the most amazing gussing game")
+level(difficulty)
+let maxValue = "" ;
+let maxNumberOfAttempts = "" ;
 
-function level() {
-    if (document.querySelector("#veryEasy").checked){
-        let maxValue = 10;
-        let maxNumberOfAttempts = 5;
-            return maxValue, maxNumberOfAttempts; // ???return??
+}
+
+function level(difficulty) {
+    if (difficulty == "1"){
+        maxValue = 10;
+        maxNumberOfAttempts = 5;
     }
-    else if (document.querySelector("#easy").checked){
-        let maxValue = 10;
-        let maxNumberOfAttempts = 5;
+    else if (difficulty == "2"){
+        maxValue = 10;
+        maxNumberOfAttempts = 5;
     }
-    else if (document.querySelector("#medium").checked){
-        let maxValue = 100;
-        let maxNumberOfAttempts = 5;
+    else if (difficulty == "3"){
+        maxValue = 100;
+        maxNumberOfAttempts = 5;
     }            
-    else if (document.querySelector("#difficult").checked){
-        let maxValue = 500;
-        let maxNumberOfAttempts = 8;
+    else if (difficulty == "4"){
+        maxValue = 500;
+        maxNumberOfAttempts = 8;
     }
-    else if (document.querySelector("#veryDifficult").checked){
-        let maxValue = 1000;
-        let maxNumberOfAttempts = 9;
+    else if (difficulty == "5"){
+        maxValue = 1000;
+        maxNumberOfAttempts = 9;
     }
     else {
         alert("Error occured. Please choose a valid diffeculty")
     }
 
-}
-
+    let min = 1;
+    max = maxValue;
+    let targetNumber =  Math.floor(Math.random() * (max - min) + min);
+    return maxValue, maxNumberOfAttempts, targetNumber
+ }
 
 
 
 function playGame() {
- console.log("play the game")
+    console.log("play the game")
+    level(difficulty)
+    keepPlaying = true;
+    userNumberOfAttempts = 0;
+    alert(`You can start guessing. the number between 1 - ${maxValue}. You have ${maxNumberOfAttempts} tries`)
+
+
 
 }
+playGame()
 
-
-
-function getUser() {
-    const name = "Nathan";
-    const role = "Web Developer";
-    return name, role;
-  }
-
-  const user = getUser();
-    console.log(user);
+main();
