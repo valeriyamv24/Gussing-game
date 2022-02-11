15c15c717191c35844dee65d4417536479a1d8c2
Valerie Martinovitch <valeriyamv24@Gmail.com>
2022-02-11 13:49:00 -0600

    modified files

--- guessGame.js
+++ guessGame.js
@@ -1,66 +1,61 @@
 function main() {
-    alert('Welcome to the most amazing gussing game')
-
+    // alert('Welcome to the most amazing gussing game')
+    // difficulty(difficultyLevel)
     let maxValue = ""; 
-    let maxNumberOfAttempts= "" ;
-
+    let maxNumberOfAttempts = "" ;
 }
 
 
 
+function difficulty(difficultyLevel){
+    console.log("start")
 
-function level() {
-
-    if (document.querySelector("#veryEasy").checked){
-        console.log("veryeasy")
+    if (difficultyLevel == "1"){
+        console.log("1")
         maxValue = 10;
         maxNumberOfAttempts = 5;
-            return maxValue, maxNumberOfAttempts;
     }
-    else if (document.querySelector("#easy").checked){
-        console.log("easy")
+    else if (difficultyLevel == "2"){
+        console.log("2")
 
         maxValue = 10;
         maxNumberOfAttempts = 5;
-            return maxValue, maxNumberOfAttempts;
-
     }
-    else if (document.querySelector("#medium").checked){
-        console.log("medium")
+    else if (difficultyLevel == "3"){
+        console.log("3")
 
         maxValue = 100;
         maxNumberOfAttempts = 5;
-            return maxValue, maxNumberOfAttempts;
-
     }            
-    else if (document.querySelector("#difficult").checked){
-        console.log("difficult")
+    else if (difficultyLevel == "4"){
+        console.log("4")
 
         maxValue = 500;
         maxNumberOfAttempts = 8;
-            return maxValue, maxNumberOfAttempts;
-
     }
-    else if (document.querySelector("#veryDifficult").checked){
-        console.log("verydifficult")
+    else if (difficultyLevel == "5"){
+        console.log("5")
+
         maxValue = 1000;
         maxNumberOfAttempts = 9;
-            return maxValue, maxNumberOfAttempts;
-
     }
     else {
-        alert("Error occured. Please choose a valid diffeculty")
+        alert("Please choose a valid diffeculty")
     }
-
+    console.log("end")
+        min = 1;
+        max = maxValue;
+        let targetNumber = Math.floor(Math.random() * (max - min + 1) + min); 
+        console.log(targetNumber)
+        return maxNumberOfAttempts, maxValue, targetNumber
 }
-// console.log(maxValue)
 
 
-
-function playGame() {
+function playGame(maxNumberOfAttempts, maxValue) {
+    
  console.log("play the game")
 
 }
 
 
-main()
+main();
