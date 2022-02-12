60298aa8b4d2a39fcdc649e85789c7ffb583fd2d
Valerie Martinovitch <valeriyamv24@Gmail.com>
2022-02-11 20:40:38 -0600

    updating files

--- guessGame.js
+++ guessGame.js
@@ -1,54 +1,52 @@
 function main() {
 alert("Welcome to the most amazing gussing game")
-level(difficulty)
+//CALL difficulty WITH difficulty_Level
+//SET difficulty_Level TO read user input 
 let maxValue = "" ;
 let maxNumberOfAttempts = "" ;
-
 }
 
-function level(difficulty) {
-    if (difficulty == "1"){
+
+function difficulty(difficultyLevel) {
+    if (difficultyLevel === "1") {
+        maxValue = 10;
+        maxNumberOfAttempts = 1000000;
+    }
+    else if (difficultyLevel === "2") {
         maxValue = 10;
         maxNumberOfAttempts = 5;
     }
-    else if (difficulty == "2"){
-        maxValue = 10;
-        maxNumberOfAttempts = 5;
-    }
-    else if (difficulty == "3"){
+    else if (difficultyLevel === "3") {
         maxValue = 100;
         maxNumberOfAttempts = 5;
-    }            
-    else if (difficulty == "4"){
+    }
+    else if (difficultyLevel === "4") {
         maxValue = 500;
         maxNumberOfAttempts = 8;
     }
-    else if (difficulty == "5"){
+    else if (difficultyLevel === "5") {
         maxValue = 1000;
         maxNumberOfAttempts = 9;
     }
     else {
-        alert("Error occured. Please choose a valid diffeculty")
+        alert("Error occured. Please choose a valid diffeculty");
     }
-
     let min = 1;
     max = maxValue;
-    let targetNumber =  Math.floor(Math.random() * (max - min) + min);
-    return maxValue, maxNumberOfAttempts, targetNumber
- }
-
+    let targetNumber = Math.floor(Math.random() * (max - min) + min);
+    console.log(targetNumber)   
+        return maxValue, maxNumberOfAttempts, targetNumber;
+    }
 
 
 function playGame() {
-    console.log("play the game")
-    level(difficulty)
+    //CALL difficulty WITH difficulty_Level RETURNING max_Number_Of_Attempts AND max_Value AND Target_Number
     keepPlaying = true;
     userNumberOfAttempts = 0;
-    alert(`You can start guessing. the number between 1 - ${maxValue}. You have ${maxNumberOfAttempts} tries`)
-
-
-
+    // alert(`You can start guessing. the number between 1 - ${maxValue}. You have ${maxNumberOfAttempts} tries`)
+    
 }
-playGame()
+
+
 
 main();
